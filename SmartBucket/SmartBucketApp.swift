1ade5583a6b86235ec8ee44156bd37be059fbcbd
MuthuGanapathy <muthuganapathy.dev@gmail.com>
2025-10-25 01:55:50 +0530

    Initial commit

---
+++ SmartBucket/SmartBucketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartBucketApp.swift
+//  SmartBucket
+//
+//  Created by Muthu on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartBucketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
