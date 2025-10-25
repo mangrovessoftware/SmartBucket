065de46355c43f7f69d50dc3f1040a0ad78b42ed
MuthuGanapathy <muthuganapathy.dev@gmail.com>
2025-10-25 13:34:04 +0530

    Add app logo and update color scheme

---
+++ SmartBucket/Utilities/Managers/AppImageManager.swift
@@ -0,0 +1,12 @@
+//
+//  AppImageManager.swift
+//  SmartBucket
+//
+//  Created by Muthu on 25/10/25.
+//
+
+import Foundation
+
+enum AppImage: String {
+    case appLogo = "AppLogo"
+}
---
+++ SmartBucket/Views/Screens/LaunchScreen.swift
@@ -0,0 +1,41 @@
+//
+//  LaunchScreen.swift
+//  SmartBucket
+//
+//  Created by Muthu on 25/10/25.
+//
+
+import SwiftUI
+
+struct LaunchScreen: View {
+    
+    var body: some View {
+        ZStack {
+            Color.gradient15
+                .ignoresSafeArea()
+            
+            logoSection
+        }
+    }
+}
+
+
+extension LaunchScreen {
+    var logoSection: some View {
+        Image("AppLogo")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 250, height: 250)
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4)
+            )
+            .shadow(radius: 7)
+    }
+}
+
+
+
+#Preview {
+    LaunchScreen()
+}
